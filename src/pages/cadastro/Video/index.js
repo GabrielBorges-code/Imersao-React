ee751c40d854833ab248df05518f571778b05e45
Gabriel <gabriel.moura2009@hotmial.com>
2020-07-28 16:52:21 -0300

    all ok

---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function CadastroVideo(){
+  return(
+    <div>
+      Pagina de cadastro de video
+    </div>
+  )
+}
+
+export default CadastroVideo;
+
+/*
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+
+function CadastroVideo() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Video</h1>
+
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CadastroVideo; 
+*/
